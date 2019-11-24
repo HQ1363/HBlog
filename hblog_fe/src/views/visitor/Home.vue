95e90ddcdb0beb6f63e88cd4974fe961d6dd0203
HQ <qiang.huang3@ymmoa.com>
2019-11-24 12:46:56 +0800

    add vender pack

---
+++ hblog_fe/src/views/visitor/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{pageName}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      pageName: '这是访客页面'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
